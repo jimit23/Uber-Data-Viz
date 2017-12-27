034d1b48e08cdf746e6c162ea996458a157011db
Jimit <34845384+jimit23@users.noreply.github.com>
2017-12-27 14:45:51 -0500

    Add files via upload

--- dataviz.py
+++ dataviz.py
@@ -8,69 +8,69 @@ import imageio
 
 geolocator = Nominatim(timeout = 50)
 
-# # ---------------------------------------------------------------------------------------------------------------------
-# # reading and saving the data
-# # maintaining a list of dictionary where the ith dictionary in the list contains the location wise count in the ith week
-# count_list = [Counter([]) for i in range(25)]
-
-# data_files = [os.path.join('uber-trip-data', filename) for filename in os.listdir('uber-trip-data') if os.path.isfile(os.path.join('uber-trip-data', filename)) and filename.endswith('.csv') and filename.startswith('uber')]
-# for filename in data_files:
-# 	with open(filename) as csvfile:
-# 		data_reader = csv.reader(csvfile, delimiter = ',')
-# 		next(data_reader)
-
-# 		for line in data_reader:
-# 			# getting the month and the date
-# 			month = int(line[0].split('/')[0])
-# 			month = month -3
-
-# 			date = int(line[0].split('/')[1])
-			
-# 			# getting the week number
-# 			if (month == 1):
-# 				week = 0
-# 			else:
-# 				week = 4*(month-1)
-
-# 			if (1<= date <= 7):
-# 				week = week + 1
-# 			elif (8<= date <= 14):
-# 				week = week + 2
-# 			elif (15<= date <= 21):
-# 				week = week + 3
-# 			else:
-# 				week = week + 4
-
-# 			lat_lon = format(float(line[1]), '.3f') + ',' + format(float(line[2]), '.3f')
-			
-# 			count_list[week] += Counter([lat_lon])
-
-# np.save("data_saved.npy", count_list)
 # ---------------------------------------------------------------------------------------------------------------------
+# reading and saving the data
+# maintaining a list of dictionary where the ith dictionary in the list contains the location wise count in the ith week
+count_list = [Counter([]) for i in range(25)]
 
-# ---------------------------------------------------------------------------------------------------------------------
-# loading the saved data and generating the images
-# count_list = np.load("data_saved.npy").tolist()
+data_files = [os.path.join('uber-trip-data', filename) for filename in os.listdir('uber-trip-data') if os.path.isfile(os.path.join('uber-trip-data', filename)) and filename.endswith('.csv') and filename.startswith('uber')]
+for filename in data_files:
+	with open(filename) as csvfile:
+		data_reader = csv.reader(csvfile, delimiter = ',')
+		next(data_reader)
 
-# week_count_dict = count_list[24]
-# lats = []
-# lons = []
+		for line in data_reader:
+			# getting the month and the date
+			month = int(line[0].split('/')[0])
+			month = month -3
 
-# for key, value in week_count_dict.items():
-# 	# unfolding the dictionary entry as a list, based on value
-# 	lat_as_float = float(key.split(',')[0])
-# 	lon_as_float = float(key.split(',')[1])
-# 	lats_temp_list = [lat_as_float for i in range(value)]
-# 	lons_temp_list = [lon_as_float for i in range(value)]
+			date = int(line[0].split('/')[1])
+			
+			# getting the week number
+			if (month == 1):
+				week = 0
+			else:
+				week = 4*(month-1)
 
-# 	# updating the weekl-based lat lon lists
-# 	lats = lats + lats_temp_list
-# 	lons = lons + lons_temp_list
+			if (1<= date <= 7):
+				week = week + 1
+			elif (8<= date <= 14):
+				week = week + 2
+			elif (15<= date <= 21):
+				week = week + 3
+			else:
+				week = week + 4
+
+			lat_lon = format(float(line[1]), '.3f') + ',' + format(float(line[2]), '.3f')
+			
+			count_list[week] += Counter([lat_lon])
+
+np.save("data_saved.npy", count_list)
+---------------------------------------------------------------------------------------------------------------------
+
+---------------------------------------------------------------------------------------------------------------------
+loading the saved data and generating the images
+count_list = np.load("data_saved.npy").tolist()
+
+week_count_dict = count_list[24]
+lats = []
+lons = []
+
+for key, value in week_count_dict.items():
+	# unfolding the dictionary entry as a list, based on value
+	lat_as_float = float(key.split(',')[0])
+	lon_as_float = float(key.split(',')[1])
+	lats_temp_list = [lat_as_float for i in range(value)]
+	lons_temp_list = [lon_as_float for i in range(value)]
+
+	# updating the weekl-based lat lon lists
+	lats = lats + lats_temp_list
+	lons = lons + lons_temp_list
 	
-# # generating the heatmap image
-# gmap = gmplot.GoogleMapPlotter(40.72, -73.886379, 11)
-# gmap.heatmap(lats, lons)
-# gmap.draw("nyc_uber_pickups-24.html")
+# generating the heatmap image
+gmap = gmplot.GoogleMapPlotter(40.72, -73.886379, 11)
+gmap.heatmap(lats, lons)
+gmap.draw("nyc_uber_pickups-24.html")
 # ---------------------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------------------
